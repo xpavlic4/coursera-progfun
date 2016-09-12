e9dfec29f5b0f434a59bab0602c0c10ca069a844
Radim Pavlicek <radim.pavlicek@gmail.com>
2016-09-12 17:11:19 +0200

    senteceOccurrences

--- forcomp/src/main/scala/forcomp/Anagrams.scala
+++ forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -41,7 +41,7 @@ object Anagrams {
   }
 
   /** Converts a sentence into its character occurrence list. */
-  def sentenceOccurrences(s: Sentence): Occurrences = ???
+  def sentenceOccurrences(s: Sentence): Occurrences = wordOccurrences(s.mkString)
 
   /** The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
    *  the words that have that occurrence count.
