9c7452fa4574afcf783dc65834f51a224819052a
Radim Pavlicek <radim.pavlicek@bmi.gv.at>
2016-09-12 13:38:36 +0200

    huffman

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -243,14 +243,22 @@ object Huffman {
     * a valid code tree that can be represented as a code table. Using the code tables of the
     * sub-trees, think of how to build the code table for the entire tree.
     */
-  def convert(tree: CodeTree): CodeTable = ???
+  def convert(tree: CodeTree): CodeTable = {
+    def acc(t: CodeTree, a: List[Bit]): CodeTable = {
+      t match {
+        case Fork(left,right,_,_) => acc(left,a :+ 0) ::: acc(right,a :+ 1)
+        case Leaf(char,_) => List((char,a))
+      }
+    }
+    acc(tree,List())
+  }
 
   /**
     * This function takes two code tables and merges them into one. Depending on how you
     * use it in the `convert` method above, this merge method might also do some transformations
     * on the two parameter code tables.
     */
-  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = ???
+  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = a:::b
 
   /**
     * This function encodes `text` according to the code tree `tree`.
@@ -258,5 +266,8 @@ object Huffman {
     * To speed up the encoding process, it first converts the code tree to a code table
     * and then uses it to perform the actual encoding.
     */
-  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = ???
+  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = {
+    val convert1: CodeTable = convert(tree)
+    text.flatMap(codeBits(convert1))
+  }
 }
